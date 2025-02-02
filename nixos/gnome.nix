6aa63cd664fbd0889c8c6df0aee27fdcb67e85e6
lunabirkeland <lunabirkeland@gmail.com>
2025-02-02 15:28:36 +0000

    CHORE: nix flake update

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -57,7 +57,7 @@
     gnomeExtensions.appindicator
   ];
 
-  services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
+  services.udev.packages = with pkgs; [gnome-settings-daemon];
 
   services.displayManager.autoLogin = {
     enable = true;
